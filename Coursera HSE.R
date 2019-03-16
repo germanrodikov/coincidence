c802c3e796466d486b7047ec336c9ac826d63e88
germanrodikov <germanrodikov@gmail.com>
2019-03-16 18:10:27 +0100

    created new file in Rstudio for tests

---
+++ Coursera HSE.R
@@ -0,0 +1,30 @@
+x <- c(23, 15, 46, NA)
+z <- c(5, 6, NA, 8)
+
+mean(x)
+mean(x, na.rm = TRUE)
+mean(z, na.rm = TRUE)
+
+sum(z, na.rm = TRUE)
+
+
+d <- data.frame (rost = x, ves = z)
+d
+
+d[4,1]
+d[,2]
+
+d[4,2]
+
+d$rost
+d[,1]
+d
+
+my_list <- list(a=7, b=10:20, table=d)
+my_list$a
+my_list$b
+my_list$table
+
+my_list[[2]]
+
+# sasa
